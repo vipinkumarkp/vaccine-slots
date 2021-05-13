9abe6a57eaf89ff5007f9d93bef30409f4d0377a
vipin <vipin@vipins-MacBook-Air.local>
2021-05-14 02:17:47 +0530

    Updates

---
+++ precache-manifest.777e0e0f9fc40aa51df185a371a7ac97.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73bd4485f2478be0fa98276b6e97b79",
+    "url": "/vaccine-slots/index.html"
+  },
+  {
+    "revision": "c1a1896568c2fa24fdd4",
+    "url": "/vaccine-slots/static/css/2.ecc8991b.chunk.css"
+  },
+  {
+    "revision": "b74113f4876764b713d2",
+    "url": "/vaccine-slots/static/css/main.fcda973f.chunk.css"
+  },
+  {
+    "revision": "c1a1896568c2fa24fdd4",
+    "url": "/vaccine-slots/static/js/2.03067bf8.chunk.js"
+  },
+  {
+    "revision": "b74113f4876764b713d2",
+    "url": "/vaccine-slots/static/js/main.24ea86c8.chunk.js"
+  },
+  {
+    "revision": "ee9412fa3e0ee61919f3",
+    "url": "/vaccine-slots/static/js/runtime~main.02cf3ee5.js"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/vaccine-slots/static/media/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/vaccine-slots/static/media/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/vaccine-slots/static/media/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/vaccine-slots/static/media/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/vaccine-slots/static/media/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/vaccine-slots/static/media/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/vaccine-slots/static/media/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/vaccine-slots/static/media/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/vaccine-slots/static/media/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/vaccine-slots/static/media/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/vaccine-slots/static/media/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/vaccine-slots/static/media/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/vaccine-slots/static/media/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/vaccine-slots/static/media/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/vaccine-slots/static/media/fa-solid-900.e8a427e1.woff2"
+  }
+]);
